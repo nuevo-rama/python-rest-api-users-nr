a59f85b54dd92691af92d8d183b2c121e3179baf
Ramiro Melero <ramiromelero@gmail.com>
2021-08-20 10:36:21 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,85 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+from users import users
+
+# Testing Route
+@app.route('/test', methods=['GET'])
+def test():
+    return jsonify({"message": "test running"})
+
+#Get Data Route
+@app.route('/users', methods=['GET'])
+def getUsers():
+    return jsonify(users)
+
+#Get Data for ID Route
+@app.route('/users/<int:id>', methods=['GET'])
+def getUserId(id):
+    print(id)
+    return 'received ID'
+
+#Get Data for Name Route
+#@app.route('/users/<string:userName>', methods=['GET'])
+#def getUserName(userName):
+  #  print(userName)
+  #  return 'received Name'
+
+#Get Data userFound Route (Find User by Name)
+@app.route('/users/<string:userName>', methods=['GET'])
+def getUser(userName):
+    userFound = [user for user in users if user['name']== userName]
+    if (len(userFound)>0):
+        return jsonify({"user": userFound[0]})
+    return jsonify ({"message": "User not found"})
+
+#POST test
+@app.route('/test', methods=['POST'])
+def addTest():
+    print(request.json)
+    return 'received'
+
+#POST Data to users (Create User)
+@app.route('/users', methods=['POST'])
+def addUser():
+    new_user = {
+        "name": request.json["name"],
+        "lastName": request.json["lastName"],
+        "age": request.json["age"]
+    }
+    users.append(new_user)
+    return jsonify({"message": "User added succesfully", "users": users})
+
+#PUT Edit user by user name
+@app.route('/users/<string:user_name>', methods=['PUT'])
+def editUser(user_name):
+    userFound = [user for user in users if user["name"] == user_name ]
+    if (len(userFound) >0):
+        userFound[0]["name"] = request.json["name"]
+        userFound[0]["lasName"] = request.json["lastName"]
+        userFound[0]["age"] = request.json["age"]
+        return jsonify({
+            "message": "User Updated",
+            "user": userFound[0]
+        })
+    return jsonify({
+        "message": "Product Not Found"
+    })
+
+#DELETE Delete user by user name
+@app.route('/users/<string:user_name>', methods=['DELETE'])
+def deleteUser(user_name):
+    userFound = [user for user in users if user["name"] == user_name ]
+    if (len(userFound) >0):
+        users.remove(userFound[0])
+        return jsonify({
+            "message": "User Deleted",
+            "user": users
+        })
+    return jsonify({
+        "message": "Product Not Found"
+    })
+
+if __name__== '__main__':
+    app.run(debug=True, port=4000)
