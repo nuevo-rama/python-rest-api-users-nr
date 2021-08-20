28b7c4ebfd6ff5a2ab29aa3a78c4cb8fb36acca0
Ramiro Melero <ramiromelero@gmail.com>
2021-08-20 10:37:40 -0300

    Create users.py

---
+++ users.py
@@ -0,0 +1,21 @@
+users = [
+    {
+        "id": 1,
+        "name": "J",
+        "lastName": "M",
+        "age": 23,
+    },
+    {
+        "id": 2,
+        "name": "A",
+        "lastName": "B",
+        "age": 24,
+    },
+    {
+        "id": 3,
+        "name": "M",
+        "lastName": "A",
+        "age": 25,
+    }
+
+]
